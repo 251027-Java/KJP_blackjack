4b954c87c64d18a6ad1e0aaa2c13ff71548a0a9e
Kenneth Vo <kennethvo2003@gmail.com>
2025-10-30 16:27:29 -0500

    added the start of the game into the code

--- src/main/java/Blackjack.java
+++ src/main/java/Blackjack.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class Blackjack {
     static final int CARD_COUNT = 14;
     static final int DEALER_MAX_LIMIT = 16;
+    static final int NUM_DECKS = 2;
 
     // two decks so 8 of each card
     static final int DECK_INIT_COUNTS = 8;
@@ -15,7 +19,8 @@ public class Blackjack {
     int userBalance;
     int userBet;
     Scanner in;
-
+    int cardsInDeck;
+    Random rand;
 
     public static void main(String[] args) {
         new Blackjack().start();
@@ -28,9 +33,57 @@ public class Blackjack {
         userBalance = 1000;
         in = new Scanner(System.in);
         Arrays.fill(deckCardCounts, DECK_INIT_COUNTS);
+        cardsInDeck = NUM_DECKS * 52;
+        rand = new Random();
     }
 
     public void start() {
+
+        boolean playerTurn = true;
+        boolean playerBust = false;
+        userBet = promptBet();
+
+        for (int i = 0; i < 2; i++) {
+            int temp = getRandomCard();
+            deckCardCounts[temp]--;
+            dealerHand.add(temp);
+
+            temp =  getRandomCard();
+            deckCardCounts[temp]--;
+            playerHand.add(temp);
+        }
+
+        int userIn = in.nextInt();
+        do {
+            int temp;
+
+            display();
+
+            switch(userIn) {
+                // hit
+                case 1:
+                    playerHand.add(temp = getRandomCard());
+
+                    int sum = 0;
+                    for (int i = 0; i < playerHand.size(); i++) sum += playerHand.get(i);
+
+                    if (sum > 21) {
+                        playerTurn = false;
+                        playerBust = true;
+                    }
+                    break;
+                // stand
+                case 2:
+                    playerTurn = false;
+                    break;
+                // int out of scope
+                default:
+                    IO.println("User input is not one of the options. Try again.");
+            }
+        } while (playerTurn);
+
+
+
         /*
         prompt user bet
 
@@ -41,6 +94,8 @@ public class Blackjack {
 
             hit - they get a new card
             stand - dealer shows card
+
+
             ---
             dealer's turn - hit until 17
             when deck runs out of cards
@@ -60,6 +115,7 @@ public class Blackjack {
 
             // display the player's and dealer's current hand
             display();
+
 
         } while (promptPlayAgain());
 
@@ -83,5 +139,16 @@ public class Blackjack {
 
     boolean promptPlayAgain() {
         return false;
+    }
+
+    /*
+    0 - N/A
+    1 - Ace
+    2-10 - 2-10
+    11-13 - Jack, Queen, King
+     */
+    int getRandomCard(){
+        int card = rand.nextInt(cardsInDeck);
+        return card;
     }
 }
