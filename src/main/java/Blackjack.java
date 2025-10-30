79b7406a5cd8ad6d8f40df2e3cf7b198205d9794
JB Ladera <minidomou@gmail.com>
2025-10-30 12:02:02 -0500

    initial thoughts on process

--- src/main/java/Blackjack.java
+++ src/main/java/Blackjack.java
@@ -1,5 +1,35 @@
-public class Blackjack {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Blackjack {
+    static final int CARD_COUNT = 13;
+    static final int DECK_INIT_COUNTS = 8;
+    static final int DEALER_MAX_LIMIT = 17;
+
+
+    public static void main(String[] args) {
+        int[] deckCardCounts = new int[CARD_COUNT];
+        Arrays.fill(deckCardCounts, DECK_INIT_COUNTS);
+
+        int userBalance = 1000;
+
+        /*
+        prompt user bet
+
+        game starts
+        - dealer shows one card (has two)
+        - user has two cards
+            user has hit or stand options
+
+            hit - they get a new card
+            stand - dealer shows card
+
+            dealer's turn - hit until 17
+            when deck runs out of cards
+
+        ace value - assume 11, if total > 21, value is 1
+        blackjack has to be a one ace and another 10-ace
+
+        TODO account for deck runs out of cards
+         */
     }
 }
