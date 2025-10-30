6cd07310adb12f73239bc311c31f5f724014fddc
PranavKoduri <61212259+PranavKoduri@users.noreply.github.com>
2025-10-30 15:31:15 -0500

    hotfix randomness compilation

--- src/main/java/Blackjack.java
+++ src/main/java/Blackjack.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Blackjack {
     static final int CARD_COUNT = 14;
     static final int DEALER_MAX_LIMIT = 16;
+    static final int NUM_DECKS = 2;
 
     // two decks so 8 of each card
     static final int DECK_INIT_COUNTS = 8;
@@ -15,7 +17,8 @@ public class Blackjack {
     int userBalance;
     int userBet;
     Scanner in;
-
+    int cardsInDeck;
+    Random rand;
 
     public static void main(String[] args) {
         new Blackjack().start();
@@ -28,6 +31,8 @@ public class Blackjack {
         userBalance = 1000;
         in = new Scanner(System.in);
         Arrays.fill(deckCardCounts, DECK_INIT_COUNTS);
+        cardsInDeck = NUM_DECKS * 52;
+        rand = new Random();
     }
 
     public void start() {
@@ -61,6 +66,7 @@ public class Blackjack {
             // display the player's and dealer's current hand
             display();
 
+
         } while (promptPlayAgain());
 
         in.close();
@@ -83,5 +89,16 @@ public class Blackjack {
 
     boolean promptPlayAgain() {
         return false;
+    }
+
+    /*
+    0 - N/A
+    1 - Ace
+    2-10 - 2-10
+    11-13 - Jack, Queen, King
+     */
+    int getRandomCard(){
+        int card = rand.nextInt(cardsInDeck);
+        return card;
     }
 }
